626dc31ce4e5abb134fc0ac4e08e59855e82b5b9
boundedrational <feli87@gmx.de>
2014-03-08 10:36:31 +0000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -60,7 +60,7 @@ for element in states
     g=0        
     if j==0
              while c<=resultslimit
-                long_url= "http://fullrss.net/a/http/rss.indeed.com/rss?q=&l=" + url_state + "california&sort=date&start="+c.to_s()
+                long_url= "http://fullrss.net/a/http/rss.indeed.com/rss?q=&l=" + url_state + "&sort=date&start="+c.to_s()
                 puts long_url
                 scraping = Nokogiri::XML(open(long_url))
                 
